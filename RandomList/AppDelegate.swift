adad5d6d9a6f65a32086ce2f24ce7dfe41a88049
Esraa <esraa.hamada@ibtikar.net.sa>
2020-03-26 14:57:19 +0200

    Initial Commit

---
+++ RandomList/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomList
+//
+//  Created by user on 3/26/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomList
+//
+//  Created by user on 3/26/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
